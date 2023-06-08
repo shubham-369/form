4d579422091de9c38a89414afb19e19ac78d060b
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-06-08 20:13:48 +0530

    bold list items

--- demo.js
+++ demo.js
@@ -3,5 +3,11 @@ title.textContent = "My lister";
 let head = document.getElementById("main-header");
 head.style.borderBottom="6px solid black";
 let item = document.getElementsByClassName("title");
-item[0].style.fontWeight= "bold";
+item[0].style.fontWeight="bold";
 item[0].style.color = "green";
+let items = document.getElementsByClassName("list-group-item");
+items[2].style.backgroundColor="green";
+
+for(i=0;i<items.length;i++){
+    items[i].style.fontWeight="bold";
+}
