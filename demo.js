66d47f9439ec8be5d36ebb651d82aec7d74524ad
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-06-08 21:57:40 +0530

    prepend text nodes

--- demo.js
+++ demo.js
@@ -1,21 +1,32 @@
-let title = document.getElementById("header-title");
-title.textContent = "My lister";
-let head = document.getElementById("main-header");
-head.style.borderBottom="6px solid black";
-let item = document.getElementsByClassName("title");
-item[0].style.fontWeight="bold";
-item[0].style.color = "green";
+// let title = document.getElementById("header-title");
+// title.textContent = "My lister";
+// let head = document.getElementById("main-header");
+// head.style.borderBottom="6px solid black";
+// let item = document.getElementsByClassName("title");
+// item[0].style.fontWeight="bold";
+// item[0].style.color = "green";
 //let items = document.getElementsByClassName("list-group-item");
 //items[2].style.backgroundColor="green";
 
-for(i=0;i<items.length;i++){
-    items[i].style.fontWeight="bold";
-}
+// for(i=0;i<items.length;i++){
+//     items[i].style.fontWeight="bold";
+// }
 //var second_item = document.querySelector(".list-group-item:nth-Child(2)");
-second_item.style.backgroundColor="green";
+//second_item.style.backgroundColor="green";
 //var third_item = document.querySelector(".list-group-item:nth-child(3)");
 //third_item.style.display="none";
-var list = document.querySelectorAll(".list-group-item:nth-child(odd)");
-for(let i=0;i<list.length;i++){
-    list[i].style.backgroundColor="green";
-}
+// var list = document.querySelectorAll(".list-group-item:nth-child(odd)");
+// for(let i=0;i<list.length;i++){
+//     list[i].style.backgroundColor="green";
+// }
+var items = document.getElementById("items");
+items.parentElement.style.backgroundColor="yellow";
+items.lastElementChild.style.backgroundColor="skyblue";
+console.log(items.lastChild)
+
+var main_title = document.getElementById("header-title");
+var text = document.createTextNode("Hello ");
+main_title.prepend(text);
+
+var lists = document.getElementsByClassName("list-group-item");
+lists[0].prepend(text)
