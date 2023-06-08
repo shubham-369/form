d846b4e7f67b503621efdec00497e0a03f2ebee7
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-06-08 19:32:39 +0530

    js update

--- demo.js
+++ demo.js
@@ -1,1 +1,7 @@
-console.log("learning git");
+let title = document.getElementById("header-title");
+title.textContent = "My lister";
+let head = document.getElementById("main-header");
+head.style.borderBottom="6px solid black";
+let item = document.getElementsByClassName("title");
+item[0].style.fontWeight= "bold";
+item[0].style.color = "green";
