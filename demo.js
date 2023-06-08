2a98826d4bfa5246f51ea69f91d6ade727e62df1
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-06-08 20:58:46 +0530

    querySelector and querySelectorAll

--- demo.js
+++ demo.js
@@ -5,9 +5,17 @@ head.style.borderBottom="6px solid black";
 let item = document.getElementsByClassName("title");
 item[0].style.fontWeight="bold";
 item[0].style.color = "green";
-let items = document.getElementsByClassName("list-group-item");
-items[2].style.backgroundColor="green";
+//let items = document.getElementsByClassName("list-group-item");
+//items[2].style.backgroundColor="green";
 
 for(i=0;i<items.length;i++){
     items[i].style.fontWeight="bold";
+}
+//var second_item = document.querySelector(".list-group-item:nth-Child(2)");
+second_item.style.backgroundColor="green";
+//var third_item = document.querySelector(".list-group-item:nth-child(3)");
+//third_item.style.display="none";
+var list = document.querySelectorAll(".list-group-item:nth-child(odd)");
+for(let i=0;i<list.length;i++){
+    list[i].style.backgroundColor="green";
 }
