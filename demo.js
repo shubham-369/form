8d93f48e692417d5c2278be397fbb0ce5773b935
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-06-09 19:10:31 +0530

    js updated for buttons

--- demo.js
+++ demo.js
@@ -18,15 +18,34 @@
 // var list = document.querySelectorAll(".list-group-item:nth-child(odd)");
 // for(let i=0;i<list.length;i++){
 //     list[i].style.backgroundColor="green";
-// }
-var items = document.getElementById("items");
-items.parentElement.style.backgroundColor="yellow";
-items.lastElementChild.style.backgroundColor="skyblue";
-console.log(items.lastChild)
+// // }
+// var items = document.getElementById("items");
+// items.parentElement.style.backgroundColor="yellow";
+// items.lastElementChild.style.backgroundColor="skyblue";
+// console.log(items.lastChild)
 
-var main_title = document.getElementById("header-title");
-var text = document.createTextNode("Hello ");
-main_title.prepend(text);
+// var main_title = document.getElementById("header-title");
+// var text = document.createTextNode("Hello ");
+// main_title.prepend(text);
 
-var lists = document.getElementsByClassName("list-group-item");
-lists[0].prepend(text)
+// var lists = document.getElementsByClassName("list-group-item");
+// lists[0].prepend(text)
+let list = document.getElementById("items");
+document.querySelector("input[type='submit']").addEventListener("click", function(e){ 
+    e.preventDefault();
+        let li = document.createElement("li");
+        let del = document.createElement("button");
+        let cross = document.createTextNode("X");
+        del.appendChild(cross);
+        del.classList.add('btn','btn-danger','btn-sm','float-right','delete');
+
+        let val = document.getElementsByClassName("form-control");
+        let item_name = document.createTextNode(val[0].value);
+        li.appendChild(item_name);
+        let atr = document.createAttribute("class");
+        atr.value="list-group-item";
+        li.setAttributeNode(atr);
+        li.appendChild(del);
+        list.appendChild(li);
+
+});
